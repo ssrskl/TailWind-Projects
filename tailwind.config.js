f1a4ab3c535ddfc01e7cb28acc7356991e62c302
ssrskl <1071352028@qq.com>
2023-08-06 10:22:59 +0800

    更新了handsome界面，使得Navbar和SideBar都固定住

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            width:{
+                '3/20': '15%',
+            }
+        },
+    },
+    plugins: [],
 }
